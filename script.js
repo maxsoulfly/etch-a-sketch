b22618462d312ea929914d082ee28c138a14562e
maxsoulfly <max.kibinimat@gmail.com>
2025-02-04 18:59:00 +0200

    added a slider, that changes grid size.

--- script.js
+++ script.js
@@ -1,27 +1,89 @@
 const container = document.querySelector('#container');
-const gridX = 16;
-const gridY = 16;
+const resize = document.querySelector('#resize');
+const slider = document.getElementById('gridSizeSlider');
+const valueDisplay = document.getElementById('gridSizeValue');
 
-const createGrid = (gridX, gridY) => {
+let gridSize = 16;
+
+slider.addEventListener('input', () => {
+  const newSize = slider.value;
+  valueDisplay.textContent = newSize;
+  updateGrid(newSize);
+});
+
+
+const updateGrid = (size) => {
+
+    container.innerHTML = ''; // Clear existing grid
+    gridSize = size;
+
+    createGrid(gridSize);
+  }
+
+/**
+ * 
+ */
+const createGrid = () => {
     // for loops to create grid
-    for(let x = 0; x < gridX; x++) {
-        for(let y = 0; y < gridY; y++) {
-            let div = document.createElement('div');
-            div.classList.add("grid-item");
-            // div.textContent = `${x},${y}`;
-            div.id = `grid-item-${x}-${y}`;
+    for(let x = 0; x < gridSize; x++) {
+        for(let y = 0; y < gridSize; y++) {
 
-            div.addEventListener('click', ()=>{
-                div.classList.toggle('painted');
+            let item = document.createElement('div');
+            item.classList.add("grid-item");
+            item.id = `grid-item-${x}-${y}`;
+
+            
+            // container
+            // container.style.width = gridSize*gridSize + "px";
+            // container.style.height = gridSize*gridSize + "px";
+
+            // grid-item
+            item.style.width = `calc(100% / ${gridSize})`;
+            item.style.height = `calc(100% / ${gridSize})`;
+            // resizeElements(div);
+
+            item.addEventListener('click', ()=>{
+                item.classList.toggle('painted');
             });
 
-            container.appendChild(div);
+            container.appendChild(item);
         }
     }
 }
 
-const togglePaint = () => {
+/**
+ * 
+ */
+const resizeElements = (gridItem) => {
+    // container
+    container.style.width = gridSize*gridSize + "px";
+    container.style.height = gridSize*gridSize + "px";
 
+    // grid-item
+    gridItem.style.width = `calc(100% / ${gridItem})`;
+    gridItem.style.height = `calc(100% / ${gridItem})`;
 }
 
-createGrid(gridX, gridY);
+/**
+ * 
+ */
+const createResizeArea = () => {
+    const p = document.createElement('p');
+    p.textContent = 'Change number of squares per side';
+    const input = document.createElement('input');
+
+    const button = document.createElement('button');
+    button.textContent = 'Click To Resize';
+
+    button.addEventListener('click', () => {
+        let newSize = parseInt(input.textContent);
+        gridSize = newSize > 100 ? newSize : 100;
+        createGrid(gridSize);
+    });
+
+    resize.appendChild(p);
+    resize.appendChild(input);
+    resize.appendChild(button);
+}
+
+updateGrid(gridSize);
